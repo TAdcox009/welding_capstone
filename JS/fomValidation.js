e25ac255027413baf381d0cb1cb8c7b3d23e3364
Raymond Hernandez <rhernandez028@west-mec.org>
2019-05-08 17:01:30 -0700

    Merge branch 'master' into gabe
    Finished Form

--- JS/fomValidation.js
+++ JS/fomValidation.js
@@ -4,24 +4,49 @@
     JS for form validaiton
 */
 
-window.onload = function () {
-    document.getElementById("submittButton").onclick = nameVal;
-    document.getElementById("submittButton").onclick = mailVal;
+// runs the function that runs the other function when the window is loaded
+window.addEventListener("load", function() {
+    document.getElementById("submittButton").onclick = runValidation;
+});
+
+
+//runs through the functions in the documnet
+function runValidation() {
+    nameVal();
+    phoneVal();
+    mailVal();
 }
 // validates the name
-function nameVal() {
+function nameVal(name) {
     var name = document.getElementById("name");
     
+    // tests if a value has been inputed and gives an error message if the input is missing
     if (name.validity.valueMissing) {
         name.setCustomValidity("Please input your first and last name");
     } else {
         name.setCustomValidity("");
     }
 }
+
+// phone number validation
+function phoneVal () {
+    var phoneNum = document.getElementById("phone");
+
+    // tests if a value has been inputed and gives an error message if the input is missing
+    if (phoneNum.validity.valueMissing) {
+        phoneNum.setCustomValidity("Please put in your phone number.");
+    } else {
+        phoneNum.setCustomValidity("");
+    }
+}
+
+
 // validate the e-mail 
 function mailVal() {
     var mail = document.getElementById("mail");
     
+
+    // tests if a value has been inputed and gives an error message if the input is missing
     if (mail.validity.valueMissing) {
         mail.setCustomValidity("Please input an email");
     } else {

582528567b056b7e912dd02294f4b9aac4f1fadf
Unknown <TAdcox009@west-mec.org>
2019-05-11 19:37:03 -0700

    animation
    added an animation that appears above the nav that says "welding technology", add js and css style rules to the animation. Applied the animation to all the pages.

---
+++ JS/animation.js
@@ -0,0 +1,11 @@
+function wordAnimation () {
+    var string = "Welding Technology";
+    var str = string.split("");
+    var el = document.getElementById("str");
+    (function animate() {
+        str.length > 0 ? el.innerHTML += str.shift( ) : clearTimeout(running);
+        var running = setTimeout(animate, 90);
+    })();
+}
+
+setTimeout(wordAnimation, 0.5);
